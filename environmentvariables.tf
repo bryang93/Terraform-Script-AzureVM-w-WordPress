cb2ccaf8a90d85014b7e1ff0881797db52b3fa59
Brayan <gudino1993@hotmail.com>
2019-10-17 15:52:50 -0400

    .

---
+++ environmentvariables.tf
@@ -0,0 +1,4 @@
+$ export ARM_CLIENT_ID="68287d6f-5ca9-40ef-916f-2d1ad0156af6"
+$ export ARM_CLIENT_SECRET="B2C2?UcvIRN6FnCXBH4?SAZSB:NZ[vqy"
+$ export ARM_SUBSCRIPTION_ID="bb1fc0b9-b4b7-4dba-a8db-d8b6e3e439d7"
+$ export ARM_TENANT_ID="https://login.microsoftonline.com/f64708dc-af27-47aa-b4c8-4465774fa9a4/oauth2/v2.0/authorize"
