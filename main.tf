a1c45940d20bbdeda970c2a89ef1cebb5990ff14
Brayan <gudino1993@hotmail.com>
2019-10-18 12:15:11 -0400

    added Variables.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "test" {
-  name     = "production"
-  location = "East US"
+  name     = "WordPress"
+  location = "eastus"
 }
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "rg" {
+    default = "WordPress"
+}
+
+variable "loc" {
+  default = "eastus"
+}
