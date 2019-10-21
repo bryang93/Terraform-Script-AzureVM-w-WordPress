3cc4ad48aa46254a64143e636888919a1392381f
Brayan <gudino1993@hotmail.com>
2019-10-21 09:55:49 -0400

    removed creds

--- provider.tf
+++ provider.tf
@@ -6,8 +6,8 @@ variable client_secret {}
 provider "azurerm" {
   version = "=1.34.0"
 
-  subscription_id = "bb1fc0b9-b4b7-4dba-a8db-d8b6e3e439d7"
-  client_id       = "68287d6f-5ca9-40ef-916f-2d1ad0156af6"
-  client_secret   = "B2C2?UcvIRN6FnCXBH4?SAZSB:NZ[vqy"
-  tenant_id       = "f64708dc-af27-47aa-b4c8-4465774fa9a4"
+  subscription_id = "XXXX"
+  client_id       = "XXXX"
+  client_secret   = "XXXX"
+  tenant_id       = "XXXX"
 }
