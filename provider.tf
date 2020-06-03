8d571962570ae732e4685543e433797d65762066
bryang93 <47666895+bryang93@users.noreply.github.com>
2020-06-03 11:19:44 -0400

    Edited credential parameters.

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
-variable subscription_id {}
-variable tenant_id {}
-variable client_id {}
-variable client_secret {}
+variable subscription_id {id here}
+variable tenant_id {tenant id here}
+variable client_id {client id here}
+variable client_secret {client secret here}
 
 provider "azurerm" {
   version = "=1.34.0"
